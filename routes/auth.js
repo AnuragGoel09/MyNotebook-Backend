9c4f181ae27864f41fcae19b9c28a9e2683c582d
AnuragGoel09 <goelanurag2003@gmail.com>
2023-10-11 18:13:15 +0530

    Login end-point with jwt-auth-token and password secuirity with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,35 @@
 const express=require('express');
 const router=express.Router();
 const User=require('../models/User');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const JWT_SECRET="MyNotebook";
 
 // Create a User using: POST "/api/auth/" . Doesn't require auth
 router.post('/createuser',async(req,res)=>{
-    
     try {
         // Check whether user email exists already
         let user = await User.findOne({email:req.body.email});
         if(user){
             return res.status(400).json({error:"Email already exist"})
         }
+
+        const salt=await bcrypt.genSalt(10);
+        const secpPass=await bcrypt.hash(req.body.password,salt);
+        // Create user
         user=await User.create({
             name:req.body.name,
-            password:req.body.password,
+            password:secpPass,
             email:req.body.email,
-        })
-        res.json(user)
+        });
+
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken=jwt.sign(data,JWT_SECRET);
+        res.json({authtoken})
         
     } catch (error) {
         console.error(error.message);
@@ -24,4 +37,30 @@ router.post('/createuser',async(req,res)=>{
     }
 })
 
+
+// Authenticate a User using POST "/api/auth/login"
+router.post('/login',async (req,res)=>{
+    
+    const {email,password}=req.body;
+    try {
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Invalid Credentials"});
+        }   
+        const passwordCompare=bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Invalid Credentials"});
+        }
+        const payload={
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken=jwt.sign(payload,JWT_SECRET);
+        return res.json({authtoken});
+
+    } catch (error) {
+        return res.status(500).send("Some Error Occured");
+    }
+});
 module.exports=router
